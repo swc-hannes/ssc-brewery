c16223e1d73d84b49861486af6ac207e7bfa9c44
Johannes Trippl <johannes.trippl@semantic-web.com>
2024-04-08 14:22:56 +0200

    mem auth provider finished

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package guru.sfg.brewery.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -36,6 +37,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+        .withUser("spring")
+        .password("{noop}guru")
+        .roles("ADMIN")
+        .and()
+        .withUser("user")
+        .password("{noop}password")
+        .roles("USER")
+        .and()
+        .withUser("scott")
+        .password("{noop}tiger")
+        .roles("CUSTOMER");
+    }
+/*    @Override
     @Bean
     protected UserDetailsService userDetailsService() {
         UserDetails admin = User.withDefaultPasswordEncoder()
@@ -51,7 +67,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .build();
 
         return new InMemoryUserDetailsManager(admin, user);
-    }
+    } */
+
+
 
 
 
